19089d018dfd1bf25b32cb66dbe501d1098ffdf3
Lomilpda <lomil97@gmail.com>
2018-10-14 16:25:20 +0300

    Update 2-12.py

--- second_lab/2-12.py
+++ second_lab/2-12.py
@@ -1,4 +1,4 @@
 import nltk
 from nltk.book import *
-average_lenght = sum([len(word) for word in text1])/len(text1)
+average_lenght = sum([len(word) for word in text1])/len(text1)])
 print(round(average_lenght, 4)
